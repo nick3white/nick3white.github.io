37ce688d16458f23d85cd7ef091beafa2dbb94bf
nick3white <nick33white@gmail.com>
2023-10-07 12:24:06 -0500

    add functional build

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,7 +1,6 @@
 import type { JSXNode } from "@builder.io/qwik";
 import { component$, Slot } from "@builder.io/qwik";
-import type { RequestHandler } from "@builder.io/qwik-city";
-
+import { useDocumentHead, type RequestHandler } from "@builder.io/qwik-city";
 // import type {  } from "@qwik-city";
 import Me from "~/media/me.png?jsx";
 import Halloween from "~/media/halloween.png?jsx";
@@ -10,97 +9,143 @@ import MappingMovement from "~/media/mapping-movement.png?jsx";
 import { css } from "~/styled-system/css";
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
-  // Control caching for this request for best performance and to reduce hosting costs:
-  // https://qwik.builder.io/docs/caching/
-  cacheControl({
-    // Always serve a cached response by default, up to a week stale
-    staleWhileRevalidate: 60 * 60 * 24 * 7,
-    // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
-    maxAge: 5,
-  });
+    // Control caching for this request for best performance and to reduce hosting costs:
+    // https://qwik.builder.io/docs/caching/
+    cacheControl({
+        // Always serve a cached response by default, up to a week stale
+        staleWhileRevalidate: 60 * 60 * 24 * 7,
+        // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
+        maxAge: 5,
+    });
 };
 
 const skills: [string, string[]][] = [
-  ["JavaScript", ["svelte", "react", "next", "node"]],
-  ["Data & Scripting", ["python", "pandas", "bash"]],
-  [
-    "Dev Ops / Sysadmin",
-    ["linux", "apache / lamp", "serverless / jamstack / CI/CD"],
-  ],
-  ["Database", ["mysql", "postgresql", "prisma", "redis"]],
+    ["JavaScript", ["Svelte", "React", "Next", "node", "(and many more)"]],
+    ["Data & Scripting", ["Python", "Pandas", "bash"]],
+    [
+        "Dev Ops / Sysadmin",
+        ["Linux", "Apache / LAMP", "Serverless / JAMstack", "CI/CD"],
+    ],
+    ["Database", ["MySQL", "PostgreSQL", "Prisma", "Redis"]],
 ];
 
 type WorkSample = [string, string, JSXNode];
 
 const samples: WorkSample[] = [
-  [
-    "Halloween",
-    "https://digital.newberry.org/collection-stories/halloween",
-    <Halloween />,
-  ],
-  ["Race B4 Race", "https://digital.newberry.org/rb4r", <RaceBeforeRace />],
-  [
-    "Mapping Movement",
-    "https://mappingmovement.newberry.org/",
-    <MappingMovement />,
-  ],
+    [
+        "Halloween",
+        "https://digital.newberry.org/collection-stories/halloween",
+        <Halloween />,
+    ],
+    ["Race B4 Race", "https://digital.newberry.org/rb4r", <RaceBeforeRace />],
+    [
+        "Mapping Movement",
+        "https://mappingmovement.newberry.org/",
+        <MappingMovement />,
+    ],
 ];
 export default component$(() => {
-  return (
-    <main
-      class={css({
-        display: "flex",
-        padding: "16px",
-        "& .sidebar": { flex: 1 },
-      })}
-    >
-      {/* sidebar  */}
-      <aside class="sidebar">
-        <section class="me-img">
-          <Me />
-        </section>
-        <section class="lists">
-          <h3 class="list-title">Skills</h3>
-          <dl class="skills-list">
-            {skills.map((list, idx: number) => (
-              <>
-                <dt key={`list-${idx}`}>{list[0]}</dt>
-                {list[1].map((l, i: number) => (
-                  <dd class="skill-dd" key={`dd-${i}`}>
-                    {l}
-                  </dd>
-                ))}
-              </>
-            ))}
-          </dl>
-          <h3 class="list-title">Samples of Recent Work</h3>
-          <dl
+    const { frontmatter } = useDocumentHead();
+    const schelector = 0;
+    const schemes = ["dark", "light", "unicorn", "synth", "flora"];
+    const scheme = schemes[schelector];
+    return (
+        <main
             class={
-              css({
-                "& img": {
-                  objectFit: "contain",
-                },
-              }) + "sample-list"
+                css({
+                    display: "flex",
+                    alignItems: "flex-start",
+                    padding: "16px",
+                    "& .sidebar": { flex: 1 },
+                }) +
+                " " +
+                scheme
             }
-          >
-            {samples.map((sample, idx) => (
-              <a key={idx} href={sample[1]} class="llines" target="_blank">
-                <dt>{sample[0]}</dt>
-                <dd>{sample[2]}</dd>
-              </a>
-            ))}
-          </dl>
-        </section>
-      </aside>
-      {/* resume  */}
-      <article class="resume-content">
-        <header>
-          <h1>Nick White</h1>
-        </header>
-        <section>
-          <Slot />
-        </section>
-      </article>
-    </main>
-  );
+        >
+            {/* sidebar  */}
+            <aside class="sidebar">
+                <section class="me-img">
+                    <Me />
+                </section>
+                <section class="lists">
+                    <h3 class="list-title">Skills</h3>
+                    <dl class="skills-list">
+                        {skills.map((list, idx: number) => (
+                            <>
+                                <div class="skill-wrap">
+                                    <dt key={`list-${idx}`}>{list[0]}</dt>
+                                    <div class="skill-liner">
+                                        {list[1].map((l, i: number) => (
+                                            <dd key={`dd-${i}`}>
+                                                {l}
+                                                {/* {l.charAt(0).toUpperCase() + */}
+                                                {/*     l.slice(1)} */}
+                                            </dd>
+                                        ))}
+                                    </div>
+                                </div>
+                            </>
+                        ))}
+                    </dl>
+                    <h3 class="list-title">Recent Work</h3>
+                    <dl
+                        class={
+                            css({
+                                "& img": {
+                                    objectFit: "contain",
+                                },
+                            }) + " sample-list"
+                        }
+                    >
+                        {samples.map((sample, idx) => (
+                            <a
+                                key={idx}
+                                href={sample[1]}
+                                class="llines"
+                                target="_blank"
+                            >
+                                <dt>{sample[0]}</dt>
+                                <dd>{sample[2]}</dd>
+                            </a>
+                        ))}
+                    </dl>
+                </section>
+            </aside>
+            {/* resume  */}
+            <article class="resume-content">
+                <header>
+                    <h1>Nick White</h1>
+                    <address>
+                        <a
+                            href={`mailto:${frontmatter.email}`}
+                            class="llines"
+                            target="_blank"
+                        >
+                            {frontmatter.email}
+                        </a>
+                        <a
+                            href={`tel:+1${frontmatter.phone}`}
+                            class="llines"
+                            target="_blank"
+                        >
+                            {frontmatter.phone}
+                        </a>
+                        <a
+                            href="http://youarelistening.to/chicago"
+                            class="llines chicago"
+                            target="_blank"
+                        >
+                            Chicago
+                            <p class="tooltip">
+                                Not my project, I just think it's cool.
+                            </p>
+                        </a>
+                    </address>
+                </header>
+                <section>
+                    <Slot />
+                </section>
+            </article>
+        </main>
+    );
 });
