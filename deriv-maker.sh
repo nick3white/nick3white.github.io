5368ba8d2932dd58affc39df73917ae9b2395500
N W <nick33white@gmail.com>
2025-07-03 12:10:43 -0500

    add portfolio

--- deriv-maker.sh
+++ deriv-maker.sh
@@ -1,7 +1,7 @@
-for file in *.png; do
+for file in images/*.png; do
   base="${file%.*}"
-  magick "$file" -resize 150x -quality 85 "${base}_thumb.webp"
-  magick "$file" -resize 400x -quality 85 "${base}_small.webp"
-  magick "$file" -resize 800x -quality 85 "${base}_medium.webp"
-  magick "$file" -resize 1200x -quality 85 "${base}_large.webp"
+  # magick "$file" -resize 150x -quality 85 "${base}_thumb.webp"
+  # magick "$file" -resize 400x -quality 85 "${base}_small.webp"
+  magick "$file" -resize 500x -quality 85 "${base}.webp"
+  # magick "$file" -resize 1200x -quality 85 "${base}_large.webp"
 done
